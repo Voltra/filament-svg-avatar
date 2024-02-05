75843b66505d83c0718f3d3d1233b7f3f7c6e9a7
Voltra <ludwig.guerin.98@gmx.fr>
2024-02-05 13:47:47 +0100

    Fix avatar override process

---
+++ src/Filament/AvatarProviders/RawSvgAvatarProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Voltra\FilamentSvgAvatar\Filament\AvatarProviders;
+
+use Filament\AvatarProviders\Contracts\AvatarProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Voltra\FilamentSvgAvatar\Contracts\SvgAvatarServiceContract;
+
+class RawSvgAvatarProvider implements AvatarProvider
+{
+    public function __construct(protected SvgAvatarServiceContract $avatarService)
+    {
+    }
+
+    public function get(Model|Authenticatable $record): string
+    {
+        return $this->avatarService->getInitials($record);
+    }
+}
